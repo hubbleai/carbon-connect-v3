395619434a0c3efe5bfb8759b5c8b616b5b23f68
costrings-frontend-dev <frontend@costrings.com>
2024-07-25 18:21:00 +0530

    add account flow

--- src/components/common/AddAccount.tsx
+++ src/components/common/AddAccount.tsx
@@ -11,6 +11,17 @@ import NoResultsIcon from "@assets/svgIcons/no-result.svg";
 import Loader from './Loader';
 import { DialogFooter } from './design-system/Dialog';
 import { pluralize } from '@utils/helper-functions';
+import FileItem from '@components/CarbonFilePicker/FileItem';
+import FileSystemAccount from './FileSystemAccount';
+
+export type MessageInfo = {
+  id: number;
+  accountBg: string;
+  accountName: string;
+  connectOn : string;
+  error:boolean
+ 
+};
 
 const AddAccount = () => {
     const [searchValue, setSearchValue] = useState<string>("");
@@ -19,9 +30,38 @@ const [selectedFiles, setSelectedFiles] = useState<number[]>([]);
 const [filesLoading, setFilesLoading] = useState(false);
 const [actionInProgress, setActionInProgress] = useState(false);
 
+const accounts: MessageInfo[] = [
+    {
+      id: 1,
+      accountBg: "#4dd2fa",
+      accountName: "Carbon",
+      connectOn:'17/11/2023',
+      error:true
+    },
+    {
+      id: 2,
+      accountBg: "#12d065",
+      accountName: "Costrings",
+      connectOn:'17/11/2023',
+      error:false
+    },
+    {
+      id: 3,
+      accountBg: "#12d065",
+      accountName: "Heeko",
+      connectOn:'17/11/2023',
+      error:false
+    },
+    {
+      id: 4,
+      accountBg: "#12d065",
+      accountName: "Hubble",
+      connectOn:'17/11/2023',
+      error:true
+    }
+  ];
 
-
-    const [files, setFiles] = useState<UserFileApi[]>([]);
+const [files, setFiles] = useState<MessageInfo[]>([]);
   return (
     <>
     <div className="cc-p-4 cc-min-h-0 cc-flex-grow cc-flex cc-flex-col">
@@ -63,7 +103,7 @@ const [actionInProgress, setActionInProgress] = useState(false);
           
         </div>
       </div>
-      <div className="cc-flex cc-justify-end cc-flex-col sm:cc-flex-row cc-text-sm cc-font-semibold cc-mb-3 cc-gap-5 sm:cc-gap-3">
+      <div className="cc-flex  cc-flex-col sm:cc-flex-row cc-text-sm cc-font-semibold cc-mb-3 cc-gap-5 sm:cc-gap-3">
         {selectedFiles.length > 0 ? (
           <button
             onClick={() => setSelectedFiles([])}
@@ -79,7 +119,8 @@ const [actionInProgress, setActionInProgress] = useState(false);
                 files.length ? selectedFiles.length === files.length : false
               }
               onCheckedChange={() => {
-                const allFilesId = files.map((item) => item.id);
+                console.log('hello')
+                const allFilesId = accounts.map((item) => item.id);
                 setSelectedFiles(allFilesId);
               }}
             />
@@ -89,24 +130,12 @@ const [actionInProgress, setActionInProgress] = useState(false);
       </div>
       <div
         id="scrollableTarget"
-        className=" dark:cc-border-[#FFFFFF1F] cc-border-t cc-flex cc-flex-col cc-border-outline-low_em cc-overflow-y-auto cc-overflow-x-hidden -cc-mx-4 cc-px-4 sm:cc-mx-0 sm:cc-px-0 cc-flex-grow sm:cc-border sm:cc-rounded-xl"
+        className="cc-flex cc-flex-col  cc-overflow-y-auto cc-overflow-x-hidden cc-mx-4 cc-px-4 sm:cc-mx-0 sm:cc-px-0 cc-flex-grow "
       >
-        <div className="cc-bg-surface-surface_1 cc-hidden sm:cc-flex dark:cc-bg-dark-border-color">
-          <div className="cc-px-4 cc-py-2 cc-text-xs cc-text-disabledtext cc-capitalize cc-font-bold cc-flex-grow dark:cc-text-dark-input-text">
-            ACCOUNT NAME
-          </div>
-        
-            <div className="cc-px-4 cc-py-2 cc-text-xs cc-text-disabledtext cc-capitalize cc-font-bold cc-flex-grow cc-text-right sm:cc-w-[100px] dark:cc-text-dark-input-text">
-              STATUS
-            </div>
-          
-          <div className="cc-py-2 cc-text-xs cc-text-disabledtext cc-capitalize cc-font-bold cc-shrink-0 cc-text-right sm:cc-w-[228px] dark:cc-text-dark-input-text">
-            <p className="cc-px-4">LAST SYNC TIME</p>
-          </div>
-        </div>
+       
         {filesLoading ? (
           <Loader />
-        ) : !files.length ? (
+        ) : files.length ? (
           <div className="cc-py-4 cc-px-4 cc-text-center cc-flex-grow cc-text-disabledtext cc-font-medium cc-text-sm cc-flex cc-flex-col cc-items-center cc-justify-center h-full">
             <div className="cc-p-2 cc-bg-surface-surface_2 cc-rounded-lg cc-mb-3">
               <img
@@ -125,28 +154,29 @@ const [actionInProgress, setActionInProgress] = useState(false);
           </div>
         ) : (
        
-            <ul className="cc-pb-2">
-              {/* {filteredList.map((item) => {
+            <div className="cc-flex cc-flex-wrap cc-gap-x-[28px]">
+              {accounts.map((item) => {
                 const isChecked = selectedFiles.indexOf(item.id) >= 0;
 
                 return (
-                  <FileItem
-                    key={item.id}
-                    isChecked={isChecked}
-                    item={item}
-                    onSelect={() => {
-                      setSelectedFiles((prev) => {
-                        if (isChecked) {
-                          return prev.filter((id) => id !== item.id);
-                        } else {
-                          return [...prev, item.id];
-                        }
-                      });
-                    }}
-                  />
+                 <FileSystemAccount
+
+                 key={item.id}
+                 isChecked={isChecked}
+                 list={item}
+                 onSelect={() => {
+                   setSelectedFiles((prev) => {
+                     if (isChecked) {
+                       return prev.filter((id) => id !== item.id);
+                     } else {
+                       return [...prev, item.id];
+                     }
+                   });
+                 }}
+                 />
                 );
-              })} */}
-            </ul>
+              })}
+            </div>
     
         )}
       </div>
---
+++ src/components/common/FileSystemAccount.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Checkbox } from './design-system/Checkbox';
+import { MessageInfo } from './AddAccount';
+import userIcon from "@assets/svgIcons/messageUser.svg";
+import calender from "@assets/svgIcons/calendar.svg";
+import error from '@assets/svgIcons/error.svg';
+import refresh from '@assets/svgIcons/refresh.svg';
+
+type PropsInfo = {
+  list: MessageInfo;
+  isChecked: boolean;
+  onSelect: () => void;
+};
+
+const FileSystemAccount = ({ list, isChecked, onSelect }: PropsInfo) => {
+  return (
+    <div className=" cc-flex cc-p-[16px_0px] cc-w-[360px] cc-border-t cc-border-[#F3F3F4] cc-justify-between">
+    <div className="cc-text-[14px] cc-flex cc-items-center cc-leading-[24px] cc-font-semibold cc-text-[#100C20]">
+      <Checkbox
+        className="cc-mr-[12px]"
+        checked={isChecked}
+        onCheckedChange={onSelect}
+      />
+      <div className='cc-flex'>
+        <div className="iconArea cc-flex cc-mr-[8px] cc-items-center">
+          <div
+            className="cc-p-[2.5px] cc-rounded-[5px] "
+            style={{ backgroundColor: list.accountBg }}
+          >
+            <img src={userIcon} alt="userIcon" />
+          </div>
+         
+        </div>
+        <span className="cc-truncate cc-w-[206px] cc-flex cc-flex-col">
+        {list.accountName}
+        <span className="cc-text-[#8C8A94] cc-leading-[16px] cc-text-[12px] cc-font-medium">{`Connected on ${list.connectOn}`}</span>
+       
+      </span>
+      </div>
+
+     
+    </div>
+    <img src={list.error? error: refresh} alt="" />
+   
+  </div>
+  )
+}
+
+export default FileSystemAccount
