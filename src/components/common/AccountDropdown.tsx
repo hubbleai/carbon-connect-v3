f28ef5b98c44d3e3bc05abcdb9b25d11d8bf852b
costrings-frontend-dev <frontend@costrings.com>
2024-08-07 12:55:11 +0530

    gix gcs issue

--- src/components/common/AccountDropdown.tsx
+++ src/components/common/AccountDropdown.tsx
@@ -100,7 +100,7 @@ export default function AccountDropdown({
             variant="gray"
             className="cc-font-semibold !cc-px-3 cc-gap-3 sm:cc-min-w-[180px] cc-rounded-xl cc-hidden sm:cc-flex cc-shrink-0  "
           >
-            <span className="cc-flex-grow cc-text-left dark:cc-text-dark-text-white">
+            <span className="cc-flex-grow cc-text-left dark:cc-text-dark-text-white cc-truncate cc-w-[148px]">
               {selectedDataSource !== null
                 ? getAccountEmail(selectedDataSource)
                 : "Connect Account"}
