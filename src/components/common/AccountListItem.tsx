0de42c35c6e5f6c77eb03fb06e5d76712db80eb5
swapnil-carbon <swapnil@carbon.ai>
2024-08-08 22:55:04 +0530

    ui fix

--- src/components/common/AccountListItem.tsx
+++ src/components/common/AccountListItem.tsx
@@ -22,14 +22,14 @@ const AccountListItem = ({
   resyncDataSource,
 }: PropsInfo) => {
   return (
-    <div className=" cc-flex cc-p-[16px_0px] md:cc-w-[100%] tab:cc-w-[100%] cc-w-[360px] cc-border-t cc-border-[#F3F3F4] cc-justify-between dark:cc-border-[#ffffff7a]">
-      <div className="cc-text-[14px] cc-flex cc-items-center cc-leading-[24px] cc-font-semibold cc-text-[#100C20]">
+    <div className=" cc-flex cc-p-[16px_0px] md:cc-w-[100%] tab:cc-w-[100%] cc-w-[360px] cc-border-t cc-border-[#F3F3F4] cc-justify-between dark:cc-border-[#ffffff7a] cc-items-center ">
+      <div className="cc-text-[14px] cc-flex cc-items-center cc-leading-[24px] cc-font-semibold cc-text-[#100C20] md:!cc-w-[90%]">
         <Checkbox
           className="cc-mr-[12px]"
           checked={isChecked}
           onCheckedChange={onSelect}
         />
-        <div className="cc-flex">
+        <div className="cc-flex md:cc-w-full">
           <div className="iconArea cc-flex cc-mr-[8px] cc-items-center">
             <div
               className="cc-p-[2.5px] cc-rounded-[5px] "
@@ -38,11 +38,10 @@ const AccountListItem = ({
               <img src={userIcon} alt="userIcon" />
             </div>
           </div>
-          <span className="cc-truncate cc-w-[206px] cc-flex cc-flex-col dark:cc-text-dark-text-white">
+          <span className="cc-break-all cc-w-[206px] cc-flex cc-flex-col dark:cc-text-dark-text-white md:!cc-w-[100%]">
             {getAccountIdentifier(account)}
-            <span className="cc-text-[#8C8A94] cc-leading-[16px] cc-text-[12px] cc-font-medium">{`Connected on ${formatDate(
-              account.created_at,
-              false
+            <span className="cc-text-[#8C8A94] cc-leading-[16px] cc-text-[12px] cc-font-medium">{`Last synced at ${formatDate(
+              account.last_synced_at
             )}`}</span>
           </span>
         </div>
