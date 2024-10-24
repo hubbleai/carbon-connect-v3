f1db928d5852e01f7cd8f8082ee01042eb541073
costrings-frontend-dev <frontend@costrings.com>
2024-10-24 16:01:40 +0530

    add tooltip for checkbox error

--- src/components/CarbonFilePicker/SourceItem.tsx
+++ src/components/CarbonFilePicker/SourceItem.tsx
@@ -5,6 +5,7 @@ import { Checkbox } from "@components/common/design-system/Checkbox";
 import { UserSourceItemApi } from "../../typing/shared";
 import { formatDate, getSourceItemType } from "../../utils/helper-functions";
 import { useCarbon } from "../../context/CarbonContext";
+import ErrorTooltip from "./ErrorTooltip";
 
 export default function SourceItem({
   item,
@@ -22,7 +23,7 @@ export default function SourceItem({
   const itemType = getSourceItemType(item);
 
   const getFileFormat = (item: UserSourceItemApi) => {
-    const parts = item.name.split(".")
+    const parts = item.name.split(".");
     if (parts.length > 1) {
       return parts[parts.length - 1].toUpperCase();
     }
@@ -44,12 +45,17 @@ export default function SourceItem({
       className="cc-flex cc-px-4 md:!cc-px-[0px] cc-transition-all cc-font-semibold cc-text-high_em cc-text-sm hover:cc-bg-gray-25 cc-cursor-pointer dark:cc-text-dark-text-white dark:hover:cc-bg-[#464646]"
     >
       <div className="cc-gap-2 cc-flex cc-items-start cc-w-full  cc-border-b cc-border-outline-base_em cc-py-3">
-        <Checkbox
-          className="cc-my-0.5"
-          checked={isChecked}
-          onCheckedChange={onSelect}
-          disabled={isItemDisabled(item)}
-        />
+        <div className="cc-relative error">
+          <Checkbox
+            className="cc-my-0.5 "
+            checked={isChecked}
+            onCheckedChange={onSelect}
+            disabled={isItemDisabled(item)}
+          />
+          <div className="cc-absolute cc-top-[32px] -cc-left-[11px] error-tooltip cc-z-[1]">
+            <ErrorTooltip leftPosTip={"12"} message={"Error Message"} />
+          </div>
+        </div>
 
         {itemType === "FOLDER" && (
           <img
